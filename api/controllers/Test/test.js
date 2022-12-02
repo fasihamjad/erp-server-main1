a7a96180c73401cb77dbf0b041adc26f66220121
fasihamjad <fasih000amjad@gmail.com>
2022-12-02 12:50:59 +0500

    jjj

--- api/controllers/Test/test.js
+++ api/controllers/Test/test.js
@@ -1,6 +1,6 @@
 exports.testFunc = async (req, res) => {
   try {
-    res.status(200).send({success: true, message: 'HELLO worldnjnjkjk!cghcghcghbbjbjhbnj bhvhvvjhbjhjh'})
+    res.status(200).send({success: true, message: 'HELLO worldnjnjkjk!cghcghcghbbjbjhbnj bhvhvvjhbjhjh trertsdfxcf chv'})
   }
   catch (err) {
     res.status(400).json({ data: "Invalid Username/Password" })
